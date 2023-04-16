5c26e3441c7f148c1177ff0a41e1e9d1899616dd
Nick Leeson <105472205+2112529@users.noreply.github.com>
2023-04-16 21:24:03 +0800

    Add files via upload

--- SIMD/Gauss_test1.cpp
+++ SIMD/Gauss_test1.cpp
@@ -3,7 +3,7 @@
 #include<time.h>
 
 #include<xmmintrin.h>
-
+#include<immintrin.h>
 #include<iostream>
 #include <bits/stdc++.h>
 //#define N 5000
@@ -59,90 +59,101 @@ float** normal_gaosi(float** matrix) //没加SSE串行的高斯消去法
 
 
 void SSE_gaosi(float** matrix) //加了SSE并行的高斯消去法
-
 {
-
     __m128 t1, t2, t3, t4;
+    for (int k = 0; k < n; k++)
+    {
+        float tmp[4] = { matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k] };
+        t1 = _mm_load_ps(tmp);
+        for (int j = n - 4; j >= k; j -= 4) //从后向前每次取四个
+        {
+            t2 = _mm_load_ps(matrix[k] + j);
+            t3 = _mm_div_ps(t2, t1);//除法
+            _mm_store_ps(matrix[k] + j, t3);
+        }
+        if (k % 4 != (n % 4)) //处理不能被4整除的元素
+        {
+            for (int j = k; j % 4 != (n % 4); j++)
+            {
+                matrix[k][j] = matrix[k][j] / tmp[0];
+            }
+        }
+        for (int j = (n % 4) - 1; j >= 0; j--)
+        {
+            matrix[k][j] = matrix[k][j] / tmp[0];
+        }
+        for (int i = k + 1; i < n; i++)
+        {
+            float tmp[4] = { matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k] };
+            t1 = _mm_load_ps(tmp);
+            for (int j = n - 4; j > k; j -= 4)
+            {
+                t2 = _mm_load_ps(matrix[i] + j);
+                t3 = _mm_load_ps(matrix[k] + j);
+                t4 = _mm_sub_ps(t2, _mm_mul_ps(t1, t3)); //减法
+                _mm_store_ps(matrix[i] + j, t4);
+            }
+            for (int j = k + 1; j % 4 != (n % 4); j++)
+            {
+                matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+            }
+            matrix[i][k] = 0;
+        }
+    }
+}
+
+void AVX_gaosi(float** matrix)
+{
+    __m256 t1, t2, t3, t4;
 
     for (int k = 0; k < n; k++)
-
     {
+        float tmp[8] = { matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k] };
+        t1 = _mm256_load_ps(tmp);
 
-        float tmp[4] = { matrix[k][k], matrix[k][k], matrix[k][k], matrix[k][k] };
-
-        t1 = _mm_loadu_ps(tmp);
-
-        for (int j = n - 4; j >= k; j -= 4) //从后向前每次取四个
-
+        for (int j = n - 8; j >= k; j -= 8)
         {
-
-            t2 = _mm_loadu_ps(matrix[k] + j);
-
-            t3 = _mm_div_ps(t2, t1);//除法
-
-            _mm_storeu_ps(matrix[k] + j, t3);
-
+            t2 = _mm256_load_ps(matrix[k] + j);
+            t3 = _mm256_div_ps(t2, t1);
+            _mm256_store_ps(matrix[k] + j, t3);
         }
 
-        if (k % 4 != (n % 4)) //处理不能被4整除的元素
-
+        if (k % 8 != (n % 8))
         {
-
-            for (int j = k; j % 4 != (n % 4); j++)
-
+            for (int j = k; j % 8 != (n % 8); j++)
             {
-
                 matrix[k][j] = matrix[k][j] / tmp[0];
-
             }
-
         }
 
-        for (int j = (n % 4) - 1; j >= 0; j--)
-
+        for (int j = (n % 8) - 1; j >= 0; j--)
         {
-
             matrix[k][j] = matrix[k][j] / tmp[0];
-
         }
 
         for (int i = k + 1; i < n; i++)
-
         {
+            float tmp[8] = { matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k] };
+            t1 = _mm256_load_ps(tmp);
 
-            float tmp[4] = { matrix[i][k], matrix[i][k], matrix[i][k], matrix[i][k] };
-
-            t1 = _mm_loadu_ps(tmp);
-
-            for (int j = n - 4; j > k; j -= 4)
-
+            for (int j = n - 8; j > k; j -= 8)
             {
-
-                t2 = _mm_loadu_ps(matrix[i] + j);
-
-                t3 = _mm_loadu_ps(matrix[k] + j);
-
-                t4 = _mm_sub_ps(t2, _mm_mul_ps(t1, t3)); //减法
-
-                _mm_storeu_ps(matrix[i] + j, t4);
-
+                t2 = _mm256_load_ps(matrix[i] + j);
+                t3 = _mm256_load_ps(matrix[k] + j);
+                t4 = _mm256_sub_ps(t2, _mm256_mul_ps(t1, t3));
+                _mm256_store_ps(matrix[i] + j, t4);
             }
 
-            for (int j = k + 1; j % 4 != (n % 4); j++)
-
+            for (int j = k + 1; j % 8 != (n % 8); j++)
             {
-
                 matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
-
             }
 
             matrix[i][k] = 0;
-
         }
-
     }
-
 }
+
 
 
 
@@ -173,78 +184,84 @@ void print(float** matrix) //输出
 int main()
 
 {
-    cin >> n;
-
-
-
-    srand((unsigned)time(NULL));
-
-    float** matrix = new float * [n];
-
-    float** matrix2 = new float * [n];
-
-    for (int i = 0; i < n; i++)
-
+    for (int i = 1; i <= 9; i++)
     {
-
-        matrix[i] = new float[n];
-
-        matrix2[i] = matrix[i];
-
-    }
+        cin >> n;
 
 
 
-    //cout << "我们生成了初始随机矩阵" << endl;
+        srand((unsigned)time(NULL));
 
-    for (int i = 0; i < n; i++)
+        float** matrix = new float* [n];
 
-    {
+        float** matrix2 = new float* [n];
 
-        for (int j = 0; j < n; j++)
+        float** matrix3 = new float* [n];
+
+        for (int i = 0; i < n; i++)
 
         {
 
-            matrix[i][j] = rand() % 100;
-            //cout << matrix[i][j] << " ";
+            matrix[i] = new float[n];
+
+            matrix3[i] = matrix2[i] = matrix[i];
+
         }
-        cout << endl;
 
+
+
+        //cout << "我们生成了初始随机矩阵" << endl;
+
+        for (int i = 0; i < n; i++)
+
+        {
+
+            for (int j = 0; j < n; j++)
+
+            {
+
+                matrix3[i][j] = rand() % 100;
+                //cout << matrix[i][j] << " ";
+            }
+            //cout << endl;
+
+        }
+
+        //print(matrix);
+
+
+
+        //cout << endl << endl << endl << "不使用SSE串行的高斯消去法" << endl;
+
+        clock_t  clockBegin, clockEnd;
+
+        //clockBegin = clock(); //开始计时
+
+        //float** B = normal_gaosi(matrix);
+
+        //clockEnd = clock();
+
+        ////print(matrix);
+
+        //cout << "总共耗时： " << clockEnd - clockBegin << "ms" << endl;
+
+        //print(B);
+
+
+
+        cout << endl << endl << endl << "使用AVX并行的高斯消去法" << endl;
+
+        clockBegin = clock(); //开始计时
+
+        SSE_gaosi(matrix3);
+
+        clockEnd = clock();
+
+        //print(matrix2);
+
+        cout << "总共耗时： " << clockEnd - clockBegin << "ms" << endl;
     }
-
-    //print(matrix);
-
-
-
-    //cout << endl << endl << endl << "不使用SSE串行的高斯消去法" << endl;
-
-    clock_t  clockBegin, clockEnd;
-
-    //clockBegin = clock(); //开始计时
-
-    //float** B = normal_gaosi(matrix);
-
-    //clockEnd = clock();
-
-    ////print(matrix);
-
-    //cout << "总共耗时： " << clockEnd - clockBegin << "ms" << endl;
-
-    //print(B);
-
-
-
-    cout << endl << endl << endl << "使用SSE并行的高斯消去法" << endl;
-
-    clockBegin = clock(); //开始计时
-
-    SSE_gaosi(matrix2);
-
-    clockEnd = clock();
-
-    //print(matrix2);
-
-    cout << "总共耗时： " << clockEnd - clockBegin << "ms" << endl;
+    
 
     system("pause");
 
